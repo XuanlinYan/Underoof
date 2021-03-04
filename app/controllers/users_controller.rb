b166dd0e0b0b3c64737d39b77595392f903e7598
jacobylee <568834144@qq.com>
2021-03-04 18:33:56 +0800

    add sign in function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,20 @@ class UsersController < ApplicationController
     def index
         @users = User.all
     end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(params.require(:user).permit(:name, :password, :email, :gender))
+
+        if @user.save
+            flash[:notice] = "Success! Welcome!"
+            redirect_to users_path
+        else
+            flash[:notice] = "Error! Please try it again"
+            redirect_to users_path
+        end
+    end
 end
