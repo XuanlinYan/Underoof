c78f72f556f2fd5c3f10a1e0f174cc8219981530
jacobylee <568834144@qq.com>
2021-03-04 13:26:02 +0800

    user view

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :conditions
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # root 'application#hello'
+  root 'users#index'
 end
