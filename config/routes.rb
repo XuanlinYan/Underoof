f96074ceafd88421e839bfb00b35aa6de5c109fd
jacobylee <568834144@qq.com>
2021-03-04 18:02:45 +0800

    Add navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :conditions
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'welcome#index'
   # root 'users#index'
 end
