232a390fa6c5004cb51ae8ecf5770d063e3ae928
XuanlinYan <xyan20@brandeis.edu>
2021-03-13 21:33:32 -0600

    Add sign up 8 fail tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ApplicationRecord
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     
     validates :name, presence: true
-    validates :password, presence: true, length: {minimum: 8}
     validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+    validates :gender, presence: true
+    validates :password, presence: true, length: {minimum: 8}
     
     # belongs_to :preference, optional:true
     # belongs_to :condition, optional:true
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,48 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "User name should be present" do
+    @user = User.new(name: "  ", gender:"Male", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
+    assert @user.valid?
+  end
+
+  test "User gender should be present" do
+    @user = User.new(name: "Example User", gender:" ", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
+    assert @user.valid?
+  end
+
+  test "User email should be present" do
+    @user = User.new(name: "Example User",gender:"Male", email: "   ", password: "foobar", password_confirmation: "foobar")
+    assert @user.valid?
+  end
+
+  test "User email should be valid" do
+    @user = User.new(name: "Example User",gender:"Male", email: "userexample.com", password: "foobar", password_confirmation: "foobar")
+    assert @user.valid?
+  end
+
+  test "User email should be unique" do
+    @user = User.new(name: "Example User", gender:"Male", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
+    duplicate_user = @user.dup
+    @user.save
+    assert duplicate_user.valid?
+  end
+
+  test "User password should be present (nonblank)" do
+    @user = User.new(name: "Example User", gender:"Male", email: "user@example.com", password: " ", password_confirmation: "foobar")
+    assert @user.valid?
+  end
+
+  test "User password should have a minimum 8 length" do
+    @user = User.new(name: "Example User",gender:"Male", email: "user@example.com", password: "foobr", password_confirmation: "foar")
+    assert @user.valid?
+  end
+
+ 
+  test "User password and password confirmation should be same" do
+    @user = User.new(name: "Example User",gender:"Male", email: "user@example.com", password: "foobar", password_confirmation: "foo2131bar")
+    assert @user.valid?
+  end
+
 end
