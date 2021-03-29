65a019ca6b0470b2cf97f10dce69da9ecb901f42
jacobylee <568834144@qq.com>
2021-03-29 16:47:10 +0800

    add error message to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
     VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{8,15}\z/
     # password should include Upper, Lower, Number. length: 8-15
     
-    validates :name, presence: true
-    validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-    validates :gender, presence: true
-    validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX }
+    validates :name, presence: {message: "cannot empty"}
+    validates :email, presence: {message: "cannot empty"}, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+    validates :gender, presence: {message: "cannot empty"}
+    validates :password, presence: {message: "is a combination of uppercase letters, lowercase letters, and numbers. And the length is 8-15"}, format: { with: VALID_PASSWORD_REGEX }
     
     has_one :preference 
 end
