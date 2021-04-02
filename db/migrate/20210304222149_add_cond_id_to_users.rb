ab4a86f0e282a809cda5b3ecdf9b311534549b89
XuanlinYan <xyan20@brandeis.edu>
2021-04-02 11:26:31 -0500

    Edit schema

--- db/migrate/20210304222149_add_cond_id_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddCondIdToUsers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :cond_id, :integer
-    add_index :users, :cond_id
-  end
-end
--- db/migrate/20210304225136_rename_user_cond_id_to_condition_id.rb
+++
@@ -1,5 +0,0 @@
-class RenameUserCondIdToConditionId < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :users, :cond_id, :condition_id
-  end
-end
