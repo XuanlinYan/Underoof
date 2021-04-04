12d3f819c67a98c1af1191771fa4d38080d10e53
XuanlinYan <xyan20@brandeis.edu>
2021-04-04 14:31:42 -0500

    Seed state, county and city and add corresponding forms

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 User.delete_all
 Preference.delete_all
 
@@ -18,3 +20,33 @@ p "Created #{User.count} users"
                         user_id: User.all.sample.id)
 end
 p "Created #{Preference.count} preference tables"
+
+
+puts "Loading States..."
+CSV.foreach(Rails.root.join("app/assets/csv/states.csv")) do |row|
+  state_code = row[0]
+  state_name = row[1]
+  State.create_with(name: state_name).find_or_create_by!(two_digit_code: state_code)
+end
+puts "...end of loading States"
+
+puts "Loading Counties..."
+CSV.foreach(Rails.root.join("app/assets/csv/counties.csv")) do |row|
+  state_code  = row[0]
+  county_name = row[1]
+  County.find_or_create_by!(state_id: State.find_by_two_digit_code(state_code).id, name: county_name)
+end
+puts "...end of loading Counties"
+
+puts "Loading Cities..."
+CSV.foreach(Rails.root.join("app/assets/csv/cities.csv")) do |row|
+  state_two_digit_code = row[0]
+  state_id = State.find_by_two_digit_code!(state_two_digit_code).id
+
+  county_name = row[1]
+  city_name   = row[2]
+  county      = County.find_by_state_id_and_name!(state_id, county_name)
+  City.find_or_create_by!(county_id: county.id, name: city_name)
+end
+
+puts "...end of loading Cities"
