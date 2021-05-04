482f7d2e58e2178f803ff60954f0b6cd3a8632e9
XuanlinYan <xyan20@brandeis.edu>
2021-05-03 20:54:35 -0500

    Fix chat message show problem

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,10 +1,11 @@
 class ChannelsController < ApplicationController
   before_action :authorize
+  before_action :correct_channel_user
   before_action :set_channel, only: %i[ show edit update destroy ]
 
   # GET /channels or /channels.json
   def index
-    @channels = Channel.all
+    @channels = current_user.channels.all
   end
 
   # GET /channels/1 or /channels/1.json
@@ -26,11 +27,9 @@ class ChannelsController < ApplicationController
 
     respond_to do |format|
       if @channel.save
-        format.html { redirect_to @channel}
-        format.json { render :show, status: :created, location: @channel }
+        format.html { redirect_to user_channels_path(current_user)}
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @channel.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -39,11 +38,9 @@ class ChannelsController < ApplicationController
   def update
     respond_to do |format|
       if @channel.update(channel_params)
-        format.html { redirect_to @channel }
-        format.json { render :show, status: :ok, location: @channel }
+        format.html { redirect_to user_channels_path(current_user)}
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @channel.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -52,8 +49,7 @@ class ChannelsController < ApplicationController
   def destroy
     @channel.destroy
     respond_to do |format|
-      format.html { redirect_to channels_url}
-      format.json { head :no_content }
+      format.html { redirect_to user_channels_path(current_user)}
     end
   end
 
@@ -66,5 +62,10 @@ class ChannelsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def channel_params
       params.require(:channel).permit(:name)
+    end
+
+    def correct_channel_user
+      @user = User.find(params[:user_id])
+      redirect_to(user_channels_path(current_user)) unless @user==current_user
     end
 end
